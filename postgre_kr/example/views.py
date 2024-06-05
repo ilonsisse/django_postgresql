af5edcfcc84160e72f9e52b6383ec4ef88e458e4
Evgenii Istomin <ilonsisse@yandex.ru>
2024-06-06 02:35:31 +0300

    views added

--- postgre_kr/example/views.py
+++ postgre_kr/example/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Place, Shop
+from .serializers import PlaceSerializer, ShopSerializer
+
+
+class PlaceViewSet(ModelViewSet):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
+
+
+class ShopViewSet(ModelViewSet):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
--- postgre_kr/postgre_kr/urls.py
+++ postgre_kr/postgre_kr/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+from example.views import PlaceViewSet, ShopViewSet
+
+router = DefaultRouter()
+router.register(prefix='shop', viewset=ShopViewSet)
+router.register(prefix='place', viewset=PlaceViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('shop/', include(router.urls)),
+    path('place/', include(router.urls))
 ]
